58aa60aa26e15923b1cf133ba527020a3d5662c9
Vinicius Martins <brunhetti-martins@hotmail.com>
2026-02-03 10:26:30 -0300

    migrations update

--- database/migrations/2026_02_03_082813_create_clients_table.php
+++ database/migrations/2026_02_03_082813_create_clients_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->nome();
+            $table->documento();
+            $table->telefone();
+            $table->email();
+            $table->endereco();
+
+
         });
     }
 
